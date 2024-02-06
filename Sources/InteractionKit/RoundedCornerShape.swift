afa36b99e9ec6fc2a2c76d2794e93326f91cf208
joakimhellgren <hellojoakim@gmail.com>
2024-02-06 20:38:31 +0100

    added rounded shape, fixed version reqs

---
+++ Sources/InteractionKit/RoundedCornerShape.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+public struct RoundedCornerShape: Shape {
+    let corners: UIRectCorner
+    let radius: CGFloat
+    
+    public init(corners: UIRectCorner, radius: CGFloat) {
+        self.corners = corners
+        self.radius = radius
+    }
+    
+    public func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
