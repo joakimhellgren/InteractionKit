8ed37cff1ae713b8ab722f772c275c17747bb7e2
joakimhellgren <hellojoakim@gmail.com>
2024-02-06 22:10:27 +0100

    nav rootview bug fix + style fixes
    There's a bug within SwiftUI which blocks the .task modifier inside VerticalDrawer from being called. To work around this issue we can pass the parent view's geometry reader proxy to VerticalDrawer, which seem to mitigate the issue.

--- Sources/InteractionKit/VerticalDrawer.swift
+++ Sources/InteractionKit/VerticalDrawer.swift
@@ -23,6 +23,7 @@ public extension BinaryFloatingPoint {
 
 
 public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarContent: View>: View {
+    private let proxy: GeometryProxy
     private let range: ClosedRange<CGFloat>
     private let preferredBarFraction: CGFloat
     
@@ -48,12 +49,14 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
     private var isDragging = false
     
     public init(
+        in proxy: GeometryProxy,
         contentFraction range: ClosedRange<CGFloat> = 0...0.5,
         barFraction: CGFloat = 0.15,
         @ViewBuilder topContent: @escaping () -> TopBarContent,
         @ViewBuilder mainContent: @escaping () -> MainContent,
         @ViewBuilder bottomContent: @escaping () -> BottomBarContent
     ) {
+        self.proxy = proxy
         self.topContent = topContent
         self.mainContent = mainContent
         self.bottomContent = bottomContent
@@ -62,11 +65,11 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
     }
     
     private var contentOpacity: CGFloat {
-        min(max(0, currentHeight.normalize(min: 0, max: maxBarHeight*0.75)), 1)
+        min(max(0, currentHeight.normalize(min: 0, max: 32)), 1)
     }
     
     private var toolbarScale: CGFloat {
-        min(max(0.5, currentHeight.normalize(min: 0, max: maxBarHeight*0.25, from: 0.5, to: 1.0)), 1)
+        min(max(0.5, currentHeight.normalize(min: 0, max: 12, from: 0.5, to: 1.0)), 1)
     }
     
     private var shadowColor: Color {
@@ -76,12 +79,9 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
     private let spacing = 0.0
     private let borderWidth = 0.5
     private let cornerRadius = 8.0
-    private let minBarHeight = 12.0
-    private let maxBarHeight = 44.0
     
+    private var size: CGSize { proxy.size }
     public var body: some View {
-        GeometryReader {
-            let size = $0.size
             VStack(spacing: spacing) {
                 Spacer()
                 topContent()
@@ -108,25 +108,24 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
                 VStack(spacing: spacing) {
                     mainContent()
                         .frame(maxWidth: size.width, maxHeight: currentHeight)
-                        .background(.ultraThinMaterial)
                         .opacity(contentOpacity)
+                        .background(.ultraThinMaterial)
                     bottomContent()
                         .zIndex(1)
                         .padding(.horizontal)
                         .frame(maxWidth: .infinity, maxHeight: preferredBarHeight(in: size))
                         .background(.bar, ignoresSafeAreaEdges: .bottom)
                         .overlay(RoundedCornerShape(
-                            corners: [.topLeft, .topRight],
+                            corners: [.bottomLeft, .bottomRight],
                             radius: cornerRadius
                         ).stroke(
-                            indicatorColor,
+                            indicatorColor.opacity(contentOpacity),
                             lineWidth: borderWidth
                         ).ignoresSafeArea(edges: .bottom))
                 }
                 
             }
             .task { resetSheet(in: size) }
-        }
     }
     
     private func preferredBarHeight(in size: CGSize) -> CGFloat {
@@ -136,9 +135,8 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
     private func dragIndicator(in size: CGSize) -> some View {
         Capsule()
             .fill(.bar)
-            .strokeBorder(indicatorColor)
-            .frame(width: maxBarHeight, height: 6)
-            //.position(x: size.width/2, y: 0)
+            .strokeBorder(indicatorColor, lineWidth: borderWidth)
+            .frame(width: 22, height: 6)
             .offset(y: -3)
             .contentShape(Rectangle())
             .gesture(onDrag(in: size))
@@ -153,7 +151,7 @@ public struct VerticalDrawer<TopBarContent: View, MainContent: View, BottomBarCo
                     let maxHeight = size.height * range.upperBound
                     let yDistance = previousHeight - gesture.translation.height
                     let height = min(max(minHeight, yDistance), maxHeight)
-                    currentHeight = max(0.5, height)
+                    currentHeight = max(0, height)
                 } else {
                     previousHeight = currentHeight
                 }
